c7d7ca64ffb03b319cd095861c2607f609beb7c2
ohoon <0hoon5661@gmail.com>
2020-09-27 15:39:00 +0900

    Clear

--- level1/크레인_인형뽑기_게임.py
+++ level1/크레인_인형뽑기_게임.py
@@ -1,3 +1,9 @@
+'''
+2019 카카오 개발자 겨울 인턴십
+크레인 인형뽑기 게임
+https://programmers.co.kr/learn/courses/30/lessons/64061
+'''
+
 def solution(board, moves):
     answer = 0
     basket = []
@@ -7,7 +13,7 @@ def solution(board, moves):
 
         for row in board:   # 위에서부터 탐색
             item = row[col]
-            
+
             if (item > 0):  # 맨 위의 인형 item
 
                 # 스택에 넣을 인형과 동일한 인형이 스택 맨 위에 존재하면
