cd0a1ca8b4e55173e2f9ec872b7395937b87daf5
ohoon <0hoon5661@gmail.com>
2020-10-03 16:04:29 +0900

    Clear

---
+++ level1/서울에서_김서방_찾기.py
@@ -0,0 +1,13 @@
+'''
+연습문제
+서울에서 김서방 찾기
+https://programmers.co.kr/learn/courses/30/lessons/12919
+'''
+
+def solution(seoul):
+    return "김서방은 {}에 있다".format(seoul.index("Kim"))
+
+'''
+여담
+레벨 1이라 너무 쉽다.............
+'''
