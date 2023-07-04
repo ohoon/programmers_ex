700ffdb7453d28fc796f4ccce8e13f6aaa8575a2
Younghoon Kang <0hoon5661@gmail.com>
2023-07-04 16:54:22 +0900

    Clear

---
+++ level0/문자열의_앞의_n글자.java
@@ -0,0 +1,11 @@
+/*
+코딩 기초 트레이닝
+문자열의 앞의 n글자
+https://school.programmers.co.kr/learn/courses/30/lessons/181907
+*/
+
+class Solution {
+    public String solution(String my_string, int n) {
+        return my_string.substring(0, n);
+    }
+}
