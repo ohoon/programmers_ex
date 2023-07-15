cb37bf11a2159e471256ebfdd1bf3caa7f223bb5
Younghoon Kang <0hoon5661@gmail.com>
2023-07-15 15:58:03 +0900

    Clear

---
+++ level0/n의_배수.java
@@ -0,0 +1,11 @@
+/*
+코딩 기초 트레이닝
+n의 배수
+https://school.programmers.co.kr/learn/courses/30/lessons/181937
+*/
+
+class Solution {
+    public int solution(int num, int n) {
+        return num % n == 0 ? 1 : 0;
+    }
+}
