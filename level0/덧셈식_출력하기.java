dfab87aafdad7d84f5cdcbeed4f085fcb3b92fdc
ohoon <0hoon5661@gmail.com>
2023-07-20 15:24:18 +0900

    Clear

---
+++ level0/덧셈식_출력하기.java
@@ -0,0 +1,17 @@
+/*
+코딩 기초 트레이닝
+덧셈식 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181947
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a+b));
+    }
+}
---
+++ level0/특수문자_출력하기.java
@@ -0,0 +1,13 @@
+/*
+코딩 기초 트레이닝
+특수문자 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181948
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+    }
+}
