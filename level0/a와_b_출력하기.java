d22d6b1b5c755dcd0a7dc6bdff536f0d83e5958c
ohoon <0hoon5661@gmail.com>
2023-07-24 17:15:17 +0900

    Clear

---
+++ level0/a와_b_출력하기.java
@@ -0,0 +1,18 @@
+/*
+코딩 기초 트레이닝
+a와 b 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181951
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
---
+++ level0/문자열_출력하기.java
@@ -0,0 +1,15 @@
+/*
+코딩 기초 트레이닝
+문자열 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181952
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
