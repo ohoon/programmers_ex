8bfca69a20cc027c831662b487b6a926c02c9f66
ohoon <0hoon5661@gmail.com>
2023-08-05 16:29:06 +0900

    Clear

---
+++ level1/명예의_전당_(1).java
@@ -0,0 +1,24 @@
+/*
+코딩테스트 연습
+명예의 전당 (1)
+https://programmers.co.kr/learn/courses/30/lessons/138477
+*/
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < score.length; i++) {
+            pq.offer(score[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+
+            answer[i] = pq.peek();
+        }
+
+        return answer;
+    }
+}
